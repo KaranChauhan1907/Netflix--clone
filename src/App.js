7d77f0c7fb8af9e23ff6a800b68642bb9b123631
KaranChauhan1907 <18bce040@nirmauni.ac.in>
2020-12-30 00:46:39 +0530

    row Complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 // c2fe80cfd4af3ab123fc4afb0c6394e8
+import Header from "./Header/Header"
+import Row from "./Row/Row"
+import requests from "./requests"
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <Row title = "Netflix Originals" fetchurl={requests.fetchNetflixOriginals}/>
+      <Row title = "Treading Now"fetchurl={requests.fetchTrending}/>
     </div>
   );
 }
---
+++ src/Row/Row.js
@@ -0,0 +1,30 @@
+import React, { useState,useEffect } from 'react'
+import instance from "../instance";
+import "./Row.css"
+const baseImgUrl = "https://image.tmdb.org/t/p/original";
+function Row({title,fetchurl}) {
+const [movies,setMovies] = useState([]);
+
+useEffect(()=>{
+    async function fetchdata(){
+        const request = await instance.get(fetchurl);
+        setMovies(request.data.results)
+        return request
+    }
+    fetchdata();
+},[fetchurl]);
+    console.log(movies)
+
+    return (
+        <div className="row">
+           <h2>{title}</h2> 
+           <div className="row_posters">
+                {movies.map(movie =>(
+                    <img className="row_poster" src={`${baseImgUrl}${movie.poster_path}`} alt={movie.name}/>
+                ))}
+           </div>
+        </div>
+    )
+}
+
+export default Row
